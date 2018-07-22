0843165b5bdc51fa7a8df591d62a36b77d8ba7ad
Mateusz <chuckstermajster@gmail.com>
2018-07-22 21:18:50 +0200

    greetings added

--- HelloWorld/app.js
+++ HelloWorld/app.js
@@ -1,1 +1,7 @@
-console.log("hesllo worlddd!!");
+console.log("starting app");
+
+const fs = require('fs');
+const os = require('os');
+
+fs.appendFileSync('greet.txt', `Hi ${os.userInfo().username}`);
+
