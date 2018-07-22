33b203a3a6da1a219cc49cabebf18a095523c3db
Mateusz <chuckstermajster@gmail.com>
2018-07-22 21:20:30 +0200

    amend message

--- HelloWorld/app.js
+++ HelloWorld/app.js
@@ -3,5 +3,5 @@ console.log("starting app");
 const fs = require('fs');
 const os = require('os');
 
-fs.appendFileSync('greet.txt', `Hi ${os.userInfo().username}`);
+fs.appendFileSync('greet.txt', `Hi ${os.userInfo().username} !`);
 
