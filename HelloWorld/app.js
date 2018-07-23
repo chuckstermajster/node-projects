f1cabf8ee76386cad59f752f0e21f23d9771c5ec
Mateusz <chuckstermajster@gmail.com>
2018-07-23 20:08:28 +0200

    node 3rd party modules (lodash)

--- HelloWorld/app.js
+++ HelloWorld/app.js
@@ -2,6 +2,20 @@ console.log("starting app");
 
 const fs = require('fs');
 const os = require('os');
-import './notes';
-fs.appendFileSync('greet.txt', `Hi ${os.userInfo().username} !`);
+const notes = require('./notes');
+const _ = require('lodash');
+
+var add = notes.add(2, 6);
+
+console.log(add);
+
+console.log(_.isString(true));
+console.log(_.isString('Mateusz'));
+
+
+
+
+//  var result = notes.addNote();
+//  console.log(result);
+//fs.appendFileSync('greet.txt', `Hi ${os.userInfo().username} ! You are ${notes.age}`);
 
